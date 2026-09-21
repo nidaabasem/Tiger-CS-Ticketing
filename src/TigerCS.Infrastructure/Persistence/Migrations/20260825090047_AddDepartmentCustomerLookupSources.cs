e7ed42689a939e0ae393879e02292a8dad24a170
nidaabasem <nidaa.basem@gmail.com>
2026-09-21 12:51:43 +0400

    fixed  confilict app setting

---
+++ src/TigerCS.Infrastructure/Persistence/Migrations/20260825090047_AddDepartmentCustomerLookupSources.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TigerCS.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentCustomerLookupSources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
